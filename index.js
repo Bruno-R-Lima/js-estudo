001c116e62caca48d12e8f3638697d44a3b1dc75
Bruno-R-Lima <hitukiri@gmail.com>
2022-11-14 18:04:27 -0300

    novo arquivo

---
+++ index.js
@@ -0,0 +1,21 @@
+
+    // EXERCÍCIO 1
+    // Faça as 4 operações básicas com base nos números 2 e 4:
+    // ao final de caada opreação imprima na tela no seguinte formanto:
+    // soma: 2+4 = x
+    // subtração: 2-4 = x
+    // divisão: 2/4 = x
+    // multiplicação = x
+    // --------------------------------------------------------
+
+    let n1 = 2;
+    let n2 = 4;
+    let soma = n1 + n2;
+    let subtração = n1 - n2;
+    let divisão = n1 / n2;
+    let multiplicação = n1 * n2;
+
+    console.log(`soma: ${n1} + ${n2} = ${soma}`);
+    console.log(`subtração:${n1} - ${n2} = ${subtração}`);
+    console.log(`divisão: ${n1} / ${n2} = ${divisão}`);
+    console.log(`multiplicação ${n1} * ${n2} = ${multiplicação}`);
