300403130852e09cd7e236b972301846844cfc16
Bruno-R-Lima <hitukiri@gmail.com>
2022-11-24 16:48:40 -0300

    renovo

--- exercicio4.js
+++
@@ -1,7 +0,0 @@
-// EXERCÍCIO 5
-// monte uma função que some dois números INT e monstre o resultado na tela!
-
-function soma(a, b){
-    return a + b;
-}
-console.log(soma(38,49));
--- exercico7.js
+++
@@ -1,4 +0,0 @@
-// pedir para o susuario digitar um número e mostrar a tabuada desse número de  1 a 1000. deve ter a possiilidade do usuário digitar outro número e também de limpar a tabuada gerada previamente.
-
-let nome =  prompt("qual é o seu nome?");
-alert("seja bem vindo" + nome);
